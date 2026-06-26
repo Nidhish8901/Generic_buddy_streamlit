30654c7654d3cfc3cb8cd17a5a54a0fdc836e6fd
Nidhish Chakradhar Reddy <nidhishmaruthi437@gmail.com>
2026-06-26 12:47:21 +0530

    Update Home.py

--- Final GenericBuddy Code/Home.py
+++ Final GenericBuddy Code/Home.py
@@ -37,7 +37,7 @@ def get_base64_image(image_path):
     with open(image_path, "rb") as img_file:
         return base64.b64encode(img_file.read()).decode()
 
-logo_base64 = get_base64_image("logo.jpeg")
+logo_base64 = get_base64_image("logo.png")
 
 # === DYNAMIC CSS BASED ON THEME ===
 st.markdown(f"""
